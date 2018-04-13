afd467e6171e9a82c27066498cac63cb9e3e034f
Ray Suyoto <ray.suy@icloud.com>
2018-04-13 10:14:55 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import NoMatch from './NoMatch';
+import NavBar from './NavBar';
+import Flash from './Flash';
+import Home from './Home';
+import { Switch, Route } from 'react-router-dom';
+import { Segment } from 'semantic-ui-react';
+
+class App extends Component {
+  render() {
+    return (
+      <Segment basic>
+        <NavBar />
+        <Flash />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route component={NoMatch} />
+          </Switch>
+      </Segment>
+    );
+  }
+}
+
+export default App;
